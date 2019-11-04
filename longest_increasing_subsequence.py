36401a8a125325f274763dcc7f4912f56f6f3a65
Lucas B <lucasbattelle@gmail.com>
2019-11-03 21:38:49 -0800

    improved and added 2 more tests

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -1,11 +1,15 @@
-def lis(i): # may have to account for special case of last digit
+#assumes that all digits are greater than or equal to 0
+# s is subsequence
+# m is an array to save subprolem solutions to
+# i is the index of the current subproblem being calculated
+def lis(i,s,m):
     if m[i] != -1:
         return m[i]   
     else:
         max_len = 1
         for j in range(i+1, len(s)):
             if s[j] > s[i]:
-                temp_len = lis(j)+1
+                temp_len = lis(j,s,m)+1
                 if max_len < temp_len:
                     max_len = temp_len
             
@@ -13,13 +17,23 @@ def lis(i): # may have to account for special case of last digit
         return max_len
 
 
-s = [11,17,5,8,6,4,7,12,3]
+def longest_increasing_subsequence(s):
+    m = [-1]* len(s) #saves subproblem solutions
+    m[len(s)-1] = 1
+    for i in range(len(s)):
+        lis(i,s,m)
+        
+    print("Longest increasing subsequence: " + str(max(m)))
+    print(m)
+    return max(m)
 
-m = [-1]* len(s)
-m[len(s)-1] = 1
 
-for i in range(len(s)):
-    lis(i)
-    
-print("Longest increasing subsequence: " + str(max(m)))
-print(m)
+sequence1 = [11,17,5,8,6,4,7,12,3]
+sequence2 = [1, 2, 3, 4, 5, 6, 7, 8,9, 10]
+sequence3 = [10,9,8,7,6,5,4,3,2,1]
+
+longest_increasing_subsequence(sequence1) #longest 4
+longest_increasing_subsequence(sequence2) #longest 10
+longest_increasing_subsequence(sequence3) #longest 1
+
+
